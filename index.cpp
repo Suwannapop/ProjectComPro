3ae641fa6bed1fde6803bbf67990b738daf4ea25
Suppakorn <suwannapop01@gmail.com>
2023-02-23 00:15:13 +0700

    ืnew index

--- index.cpp
+++ index.cpp
@@ -5,18 +5,8 @@
 
 using namespace std;
 
-struct user
-{
-    string ID;
-    string pass;
-    double money;
-};
 int main()
 {
-    user u1 = {"650612100", "2100", 1000};
-    user u2 = {"650612082", "2082", 1000};
-    double MoneyData[] = {};
-    MoneyData[0] = u1.money;
-    MoneyData[1] = u2.money;
-
+   fstream data ;
+   data.open("Datafile.txt"); //อันนี้เเล้วเเต่คนว่าจะเก็บไฟล์Data ไว้ที่ไหนนะ
 }
