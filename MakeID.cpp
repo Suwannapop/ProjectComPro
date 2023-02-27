26b3830f3b37f896a5d802bc4a81f167333f5836
Delphisss <Kong_led_@hotmail.com>
2023-02-27 12:36:53 +0700

    MakrID

---
+++ MakeID.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+
+int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 9);
+    for (int i = 650610000; i <= 650615000; i++) {
+        std::string id = std::to_string(i);
+        std::string filename = std::to_string(i) + ".txt";
+        std::ofstream outfile(filename);
+        if (outfile.is_open()) {
+            int num = dist(gen) * 1000;
+            outfile << id << "," << num << ".00";
+            outfile.close();
+            std::cout << "File created: " << id << std::endl;
+        } else {
+            std::cout << "Error creating file: " << id << std::endl;
+        }
+    }
+    return 0;
+}
+
