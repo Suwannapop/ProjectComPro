04b902dc1bc56e814661fd649bb6fe7384e48cf6
Delphisss <Kong_led_@hotmail.com>
2023-02-27 14:19:59 +0700

    ทำไอดีแบบชั่วคราว

--- MakeID.cpp
+++ MakeID.cpp
@@ -7,13 +7,14 @@ int main() {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(1, 9);
-    for (int i = 650610000; i <= 650615000; i++) {
+    for (int i = 650610000; i <= 650610001; i++) {
+        int id_user = 650610000;
         std::string id = std::to_string(i);
         std::string filename = std::to_string(i) + ".txt";
         std::ofstream outfile(filename);
         if (outfile.is_open()) {
             int num = dist(gen) * 1000;
-            outfile << id << "," << num << ".00";
+            outfile << id << "," << num << ".00" << "," << i-id_user;
             outfile.close();
             std::cout << "File created: " << id << std::endl;
         } else {
