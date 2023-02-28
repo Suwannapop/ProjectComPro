57ad68b00a3d13cd9b5a4c9ad3fc394f48ef68f5
D-Delphis <kong_led_@hotmail.com>
2023-03-01 00:16:49 +0700

    Bug deposit

--- ATM-UI.cpp
+++ ATM-UI.cpp
@@ -19,6 +19,126 @@ struct Dataformat
     vector<float> money;
 };
 
+//deposit
+void deposit(Dataformat &ID1 );
+void ImportFile(Dataformat &ID, string fileindex);
+
+int deposit()
+{
+	Dataformat ID1;
+    
+    string id;
+    float amount;
+    cout << "input your id : ";
+    cin >> id;
+
+    string userfile_1 = id+".txt";
+    ofstream writefile_1;
+    writefile_1.open(userfile_1);
+    writefile_1 << "2100"<<","<<"650612100" << "," << "10000" << endl;
+    writefile_1.close();
+
+    ImportFile(ID1, userfile_1); 
+	deposit(ID1);
+
+	writefile_1.open(userfile_1);
+    writefile_1 <<ID1.Pass[0]<<","<< ID1.ID[0] << "," << ID1.money[0]<< endl;
+    writefile_1.close();
+}
+
+void ImportFile(Dataformat &ID, string fileindex)
+{
+    ifstream data;
+    data.open(fileindex);
+    string line;
+    int idnumber, pass;
+    float balnace;
+    if (data.is_open())
+    {
+        while (getline(data, line)) // importfile
+        {
+            sscanf(line.c_str(), "%d,%d,%f", &pass, &idnumber, &balnace);
+            ID.Pass.push_back(pass);
+            ID.ID.push_back(idnumber);
+            ID.money.push_back(balnace);
+        }
+    }else {
+        cout << "Don't have ID : " << fileindex << "in datauser" ;
+    }
+}
+
+void deposit(Dataformat &ID1 ){
+
+	int option = 0;	
+	float amount;
+	bool notfinished = true;
+
+	do {
+		cout << "How much money do you want to deposit? :" << endl;
+		cout << "1 - $20" << endl;
+		cout << "2 - $50" << endl;	
+		cout << "3 - $100" << endl;
+		cout << "4 - $200" << endl;
+		cout << "5 - $500" << endl;
+		cout << "6 - choose your own deposition amount" << endl;
+		cout << "7 - cancal deposition \n";
+		cout << "choose a deposit option (1-7)" << endl;
+		cin >> option;
+
+		switch (option) {
+			case 1:
+				ID1.money[0] = ID1.money[0] + 20;
+				cout << "Successfully deposited $20 to your account. Current balance is $" << ID1.money[0] << "." << endl;
+				notfinished = false;
+				break;
+			case 2:
+				ID1.money[0] = ID1.money[0] + 50;
+				cout << "Successfully deposited $50 to your account. Current balance is $" << ID1.money[0] << "." << endl;
+				notfinished = false;
+				break;
+			case 3:
+				ID1.money[0] = ID1.money[0] + 100;
+				cout << "Successfully deposited $100 to your account. Current balance is $" << ID1.money[0] << "." << endl;
+				notfinished = false;
+				break;
+			case 4:
+				ID1.money[0] = ID1.money[0] + 200;
+				cout << "Successfully deposited $200 to your account. Current balance is $" << ID1.money[0] << "." << endl;
+				notfinished = false;
+				break;
+			case 5:
+				ID1.money[0] = ID1.money[0] + 500;
+				cout << "Successfully deposited $500 to your account. Current balance is $" << ID1.money[0] << "." << endl;
+				notfinished = false;	
+				break;
+			case 6:
+				cout << "How manny do you want to Deposit." ;
+				cin >> amount;
+				if (amount > 0)
+				{
+
+					ID1.money[0] = ID1.money[0] + amount;
+					cout << "Successfully deposited $500 to your account. Current balance is $" << ID1.money[0] << "." << endl;
+					notfinished = false;
+					break;
+				}else {
+					cout << "Invalid! Please try again." << endl;
+					Sleep(1000);
+					break;
+				}
+				
+			case 7:
+				cout << "Deposition canceled." << endl;
+				notfinished = false;
+				break;
+			default:
+				cout << "Invalid! Please try again." << endl;
+				Sleep(1000);
+				break;
+		} 
+	} while (notfinished);
+
+}
 //higlight_yes_or_No
 void higlight_Yes_or_No(int index, bool selected) { 
     int l = 0 ;
@@ -263,22 +383,6 @@ int main(){
     login(id , password);
     string fileindex = id + ".txt" ;
     ImportFile(ID1, fileindex);
-/*///////////////////////////////////////////////////////////////////////////////////
-    //pass chkce
-    int value1;
-    ifstream inFile(id+".txt"); // open the file for reading
-    if (inFile.is_open()) { // check if the file was successfully opened
-        //inFile.ignore(1000, ','); // ignore the first value
-        //inFile.ignore(1000, ','); // ignore the second value
-        inFile >> value1; // read the third value
-        inFile.close(); // close the file
-        pass_id =  value1 ;
-    } else {
-        Login_failed();
-        Sleep(1500);
-        main();
-    }
-///////////////////////////////////////////////////////////////////////////////////*/
     ifstream myfile;
     myfile.open(id + ".txt");
     
@@ -334,6 +438,9 @@ int main(){
 
                     case 2:
                         // Deposit
+                        deposit();
+                        
+                        /*
                         system("cls"); // clear the console
                         cout << "+-------------------------------------+\n";
                         cout << "|               Deposit               |\n"; 
@@ -362,7 +469,7 @@ int main(){
                                     cout << "Invalid deposit amount.\n";
                                     Sleep(4000); // Wait for 1000 milliseconds
                                     main();
-                                }
+                                }*/
                                 break;
                     
                     case 3:
