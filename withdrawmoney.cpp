44ca205c863b849d82065e94be4c1bf2ba08cca6
mart <santhod_kh@cmu.ac.th>
2023-03-02 21:34:40 +0700

    wd_ok

--- withdrawmoney.cpp
+++ withdrawmoney.cpp
@@ -2,83 +2,68 @@
 #include <cmath>
 #include <fstream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
-void Withdraw(double , vector<float> &);
-void ImportFile(vector<int> &, vector<float> &, string );
+void Withdraw(double, vector<float> &);
+void ImportFile(vector<int> &, vector<float> &, string);
 struct Dataformat
 {
+    vector<int> Pass;
     vector<int> ID;
-    vector<float> money;
+    vector<double> money;
 };
 
-int main()
+int mainWithdraw(Dataformat ID , string userfile_1)
 {
-    Dataformat ID;
-    Dataformat money;
-    string id;
     float amount;
-    cout << "input your id : ";
-    cin >> id;
-
-    string userfile_1 = id+".txt";
-
     ofstream writefile_1;
-    writefile_1.open(userfile_1);
-
-    writefile_1 << "650612100"
-    << ","
-    << "10000" << endl;
-
-    writefile_1.close();
-
-
-    ImportFile(ID.ID, money.money,userfile_1); 
-
-
-
+    ImportFile(ID,userfile_1);
     cout << "Enter amount : ";
     cin >> amount;
-    Withdraw(amount ,money.money);
-    cout << "withdraw : " << amount ;
-    cout << "\nNow : ID : " <<ID.ID[0] << ", total balance is : " << money.money[0]<< endl;
+    Withdraw(amount, ID);
+    cout << "withdraw : " << amount;
+    cout << "\nNow : ID : " << ID.ID[0] << ", total balance is : " <<setprecision(1000000)<< ID.money[0] << endl;
 
     writefile_1.open(userfile_1);
-
-    writefile_1 << ID.ID[0]
-    << ","
-    << money.money[0]<< endl;
-
+    writefile_1 <<ID.Pass[0]<< ID.ID[0]<< ","<< ID.money[0] << endl;
     writefile_1.close();
-
 }
 
-void ImportFile(vector<int> &ID, vector<float> &money , string fileindex)
+void ImportFile(Dataformat &ID, string fileindex)
 {
     ifstream data;
     data.open(fileindex);
     string line;
-    int idnumber;
-    float balnace;
-    while (getline(data, line)) // importfile
+    int idnumber, pass;
+    double balnace;
+    if (data.is_open())
     {
-    sscanf(line.c_str(), "%d,%f", &idnumber, &balnace);
-    ID.push_back(idnumber);
-    money.push_back(balnace);
+        while (getline(data, line)) // importfile
+        {
+            sscanf(line.c_str(), "%d,%d,%lf", &pass, &idnumber, &balnace);
+            ID.Pass.push_back(pass);
+            ID.ID.push_back(idnumber);
+            ID.money.push_back(balnace);
+        }
+    }else {
+        cout << "Don't have ID : " << fileindex << "in datauser" ;
     }
 }
 
-void Withdraw(double amount, vector<float> &money ) 
+void Withdraw(double amount, Dataformat &ID)
 {
-    if (amount > money[0])
+    if (amount > 0)
     {
-        cout << "You don't have enough money\n";
-    cout << "Now you have : " << money[0] << " You can't withdraw money\n";
-    }
-    else
-    {
-    money[0] = money[0] - amount;
-
+        if (amount > ID.money[0])
+        {
+            cout << "You don't have enough money\n";
+            cout << "Now you have : " << setprecision(1000000) << ID.money[0] << " You can't withdraw money\n";
+        }
+        else
+        {
+            ID.money[0] = ID.money[0] - amount;
+        }
     }
 }
