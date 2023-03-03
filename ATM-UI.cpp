af661125db9a60992bbd33a8b846fb4b79361321
Suppakron <suwannapop01@gmail.com>
2023-03-03 12:23:35 +0700

    add Withdraw

--- ATM-UI.cpp
+++ ATM-UI.cpp
@@ -643,6 +643,49 @@ int login(string &id, int &password)
     }
 }
 
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+void Withdraw(double, vector<float> &);
+int mainWithdraw(Dataformat ID , string userfile_1)
+{
+    float amount;
+    ofstream writefile_1;
+    ImportFile(ID,userfile_1);
+    cout << "Enter amount : ";
+    cin >> amount;
+    Withdraw(amount, ID);
+    cout << "withdraw : " << amount;
+    cout << "\nNow : ID : " << ID.ID[0] << ", total balance is : " <<setprecision(1000000)<< ID.money[0] << endl;
+
+    writefile_1.open(userfile_1);
+    writefile_1 <<ID.Pass[0]<< ID.ID[0]<< ","<< ID.money[0] << endl;
+    writefile_1.close();
+}
+void Withdraw(double amount, Dataformat &ID)
+{
+    if (amount > 0)
+    {
+        if (amount > ID.money[0])
+        {
+            cout << "You don't have enough money\n";
+            cout << "Now you have : " << setprecision(1000000) << ID.money[0] << " You can't withdraw money\n";
+        }
+        else
+        {
+            ID.money[0] = ID.money[0] - amount;
+        }
+    }
+}
+
+
+
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
 int main()
 {
     int password;
