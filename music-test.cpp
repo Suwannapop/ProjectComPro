0f30b1e7389daa66f19052ec465c08e51460118d
D-Delphis <kong_led_@hotmail.com>
2023-03-09 14:20:13 +0700

    audio

--- music-test.cpp
+++ music-test.cpp
@@ -1,50 +1,11 @@
-#include "miniaudio.h"
+#include <iostream>
+#include <windows.h>
+#include <mmsystem.h>
 
-int main(int argc, char** argv) {
-    ma_result result;
-    ma_decoder decoder;
-    ma_device_config deviceConfig;
-    ma_device device;
-
-    result = ma_decoder_init_file("Onii_chan_sound.wav", NULL, &decoder);
-    if (result != MA_SUCCESS) {
-        printf("Failed to open audio file.\n");
-        return -1;
-    }
-
-    deviceConfig = ma_device_config_init(ma_device_type_playback);
-    deviceConfig.playback.format   = decoder.outputFormat;
-    deviceConfig.playback.channels = decoder.outputChannels;
-    deviceConfig.sampleRate        = decoder.outputSampleRate;
-    deviceConfig.dataCallback      = playback_callback;
-    deviceConfig.pUserData         = &decoder;
-
-    result = ma_device_init(NULL, &deviceConfig, &device);
-    if (result != MA_SUCCESS) {
-        printf("Failed to initialize playback device.\n");
-        ma_decoder_uninit(&decoder);
-        return -1;
-    }
-
-    result = ma_device_start(&device);
-    if (result != MA_SUCCESS) {
-        printf("Failed to start playback device.\n");
-        ma_device_uninit(&device);
-        ma_decoder_uninit(&decoder);
-        return -1;
-    }
-
-    printf("Playing audio file. Press Enter to quit...\n");
-    getchar();
-
-    ma_device_stop(&device);
-    ma_device_uninit(&device);
-    ma_decoder_uninit(&decoder);
-
+int main(int argc, char** argv)
+{
+    PlaySound(TEXT("D:\\compro\\projcet_TT\\ProjectComPro\\Onii_chan_sound.wav"),NULL,SND_SYNC);
     return 0;
 }
 
-static ma_uint32 playback_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
-    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
-    return (ma_uint32)ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount);
-}
+//D:\\compro\\projcet_TT\\ProjectComPro\\Onii_chan_sound.wav
