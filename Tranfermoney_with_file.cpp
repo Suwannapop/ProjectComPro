c28d65b49c914a493feebabe2ef0148b15ab6f51
Okinawa <fitthep@gmail.com>
2023-03-10 11:50:38 +0700

    first draft

--- Tranfermoney_with_file.cpp
+++ Tranfermoney_with_file.cpp
@@ -8,32 +8,31 @@
 using namespace std;
 struct Dataformat
 {
-    vector<int> Pass;
     vector<int> ID;
     vector<double> money;
 };
 
-void TransferMoney(double amount, Dataformat &ID1, Dataformat &ID2);
+void topup(double amount, Dataformat &ID1, Dataformat &ID2);
 void ImportFile(Dataformat &ID, string fileindex);
 
 int main()
 {
     Dataformat ID1;
     Dataformat ID2;
-    string id, IdTranfer;
+    string id, IdTopup;
     double amount;
     cout << "input your id : ";
     cin >> id;
     string userfile_1 = id + ".txt";
     ImportFile(ID1, userfile_1);
     cout << "What ID you want to tranfer : ";
-    cin >> IdTranfer;
-    string userfile_2 = IdTranfer + ".txt";
+    cin >> IdTopup;
+    string userfile_2 = IdTopup + ".txt";
     ofstream writefile_1, writefile_2; // importfile
     ImportFile(ID2, userfile_2);
     cout << "How much money : ";
     cin >> amount;
-    TransferMoney(amount, ID1, ID2);
+    topup(amount, ID1, ID2);
     cout << "You Tranfer Money From ID To ID \n"
          << ID1.ID[0] << " -----> " << ID2.ID[0] << endl;
     cout << "Now : ID : " << ID1.ID[0] << " Is " << ID1.money[0] << endl;
@@ -50,25 +49,25 @@ void ImportFile(Dataformat &ID, string fileindex)
     ifstream data;
     data.open(fileindex);
     string line;
-    int idnumber, pass;
+    int idnumber;
     double balnace;
     string name = fileindex ;
     if (data.is_open())
     {
         while (getline(data, line)) // importfile
         {
-            sscanf(line.c_str(), "%d,%d,%lf", &pass, &idnumber, &balnace);
-            ID.Pass.push_back(pass);
+            sscanf(line.c_str(), "%d,%lf", &idnumber, &balnace);
+
             ID.ID.push_back(idnumber);
             ID.money.push_back(balnace);
         }
     }else {
-        cout << "Don't have ID : " << name << " in datauser \n" ;
+        cout << "Don't have ID : " << name << " in database \n" ;
         main();
     }
 }
 
-void TransferMoney(double amount, Dataformat &ID1, Dataformat &ID2) // เเปปปกติ
+void topup(double amount, Dataformat &ID1, Dataformat &ID2) 
 {
     if (amount > ID1.money[0])
     {
