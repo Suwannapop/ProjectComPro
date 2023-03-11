828d00c8b2c54125c4c3c3535d378b6ac6e44122
Okinawa <fitthep@gmail.com>
2023-03-11 17:00:39 +0700

    In progress

--- Gametopup.cpp
+++ Gametopup.cpp
@@ -8,8 +8,10 @@
 using namespace std;
 struct Dataformat
 {
+    vector<int> Pass;
     vector<int> ID;
     vector<double> money;
+    string name;
 };
 
 void topup(double amount, Dataformat &ID1, Dataformat &ID2);
@@ -25,7 +27,7 @@ int main()
     cin >> id;
     string userfile_1 = id + ".txt";
     ImportFile(ID1, userfile_1);
-    cout << "What ID you want to tranfer : ";
+    cout << "What Game ID you want to top up: ";
     cin >> IdTopup;
     string userfile_2 = IdTopup + ".txt";
     ofstream writefile_1, writefile_2; // importfile
@@ -33,14 +35,14 @@ int main()
     cout << "How much money do you want to top up: ";
     cin >> amount;
     topup(amount, ID1, ID2);
-    cout << "You top up money from ID To ID \n"
-         << ID1.ID[0] << " -----> " << ID2.ID[0] << endl;
+    cout << "You top up money from ID to ID \n"
+         << ID1.name << " -----> " << ID2.name << endl;
     cout << "Now : ID : " << ID1.ID[0] << " is " << ID1.money[0] << endl;
     cout << "Now : ID : " << ID2.ID[0] << " is " << ID2.money[0] << endl;
     writefile_1.open(userfile_1);
     writefile_2.open(userfile_2);
-    writefile_1 << ID1.ID[0] << "," << setprecision(10000)<< ID1.money[0] << endl;
-    writefile_2 << ID2.ID[0] << "," << setprecision(10000)<< ID2.money[0] << endl;
+    writefile_1 << ID1.Pass[0] << "," << ID1.ID[0] << "," << setprecision(10000) << ID1.money[0] << ","<< ID1.name << endl;
+    writefile_2 << ID2.Pass[0] << "," << ID2.ID[0] << "," << setprecision(10000) << ID2.money[0] << ","<< ID2.name << endl;
     writefile_1.close();
     writefile_2.close();
 }
@@ -50,22 +52,19 @@ void ImportFile(Dataformat &ID, string fileindex)
     ifstream data;
     data.open(fileindex);
     string line;
-    int idnumber;
+    int idnumber, pass;
     double balnace;
-    string name = fileindex ;
-    if (data.is_open())
-    {
-        while (getline(data, line)) // importfile
-        {
-            sscanf(line.c_str(), "%d,%lf", &idnumber, &balnace);
-
-            ID.ID.push_back(idnumber);
-            ID.money.push_back(balnace);
+    char name[99];
+     if (data.is_open()){
+            while (getline(data, line)) // importfile
+            {
+                sscanf(line.c_str(), "%d,%d,%lf,%s", &pass, &idnumber, &balnace, name);
+                ID.Pass.push_back(pass);
+                ID.ID.push_back(idnumber);
+                ID.money.push_back(balnace);
+                ID.name = name;
+            }
         }
-    }else {
-        cout << "Don't have ID : " << name << " in database \n" ;
-        main();
-    }
 }
 
 void topup(double amount, Dataformat &ID1, Dataformat &ID2) 
